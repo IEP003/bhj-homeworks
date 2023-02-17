0b6d670ae11d7fd9dc5a19c4e3cc9b7432756d86
Igor <igor@i-petchenko.ru>
2023-02-17 10:58:55 +0300

    first change tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,13 +2,12 @@ const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'))
 let tooltip = document.createElement('div')
 tooltip.classList.add('tooltip')
 
-
 hasTooltip.forEach(item => {
-    let place = item.getBoundingClientRect();
-    let left = place.left;
-    let top = place.bottom;
     item.addEventListener('click', (evt) => {
         evt.preventDefault();
+        let place = item.getBoundingClientRect();
+        let left = place.left;
+        let top = place.bottom;
         tooltip.innerHTML = item.title;
         tooltip.classList.toggle('tooltip_active')
         item.insertAdjacentElement('beforeend', tooltip)
